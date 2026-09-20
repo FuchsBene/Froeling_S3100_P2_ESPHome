558d5c7381d96192aedb651512ea663749b740c3
Benedikt Fuchs <91484898+FuchsBene@users.noreply.github.com>
2026-09-20 13:07:35 +0200

    Remove example configuration and add file header to __init__.py

--- esphome/components/froeling_s3100_p2/__init__.py
+++ esphome/components/froeling_s3100_p2/__init__.py
@@ -1,29 +1,27 @@
+'''
+    * __init__.py
+    * Config validation and code generation for the ESPHome component.
+    *     
+    * Froeling_S3100_P2_ESPHome
+    * Copyright (c) 2026 Benedikt Fuchs <github.com/FuchsBene>
+'''
+
 import esphome.config_validation as cv
 import esphome.codegen as cg
 from esphome.components import uart
 from esphome.const import CONF_ID
 
-#CONF_FOO = "foo"
-#CONF_BAR = "bar"
-#CONF_BAZ = "baz"
 
 froeling_component_ns = cg.esphome_ns.namespace("froeling_s3100_p2")
 FroelingComponent = froeling_component_ns.class_("FroelingComponent", cg.Component, uart.UARTDevice)
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(FroelingComponent),
-    #cv.Required(CONF_FOO): cv.boolean,
-    #cv.Optional(CONF_BAR): cv.string,
-    #cv.Optional(CONF_BAZ): cv.int_range(0, 255),
 }).extend(uart.UART_DEVICE_SCHEMA).extend(cv.COMPONENT_SCHEMA)
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await uart.register_uart_device(var, config)
 
-    #cg.add(var.set_foo(config[CONF_FOO]))
-    #if bar := config.get(CONF_BAR):
-    #    cg.add(var.set_bar(bar))
-    #if baz := config.get(CONF_BAZ):
-    #    cg.add(var.set_baz(baz))
+    await uart.register_uart_device(var, config)
